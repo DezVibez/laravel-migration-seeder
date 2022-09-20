9630a7eac7778c746e7f65989b6e3ce79d6a1abc
Emanuele <103491766+DezVibez@users.noreply.github.com>
2022-09-20 19:16:37 +0200

    migrazione lanciata, creata tabella trains su php my admin

--- database/migrations/2022_09_20_160541_create_trains_table.php
+++ database/migrations/2022_09_20_160541_create_trains_table.php
@@ -15,7 +15,17 @@ class CreateTrainsTable extends Migration
     {
         Schema::create('trains', function (Blueprint $table) {
             $table->id();
+            $table->string('Azienda', 20);
+            $table->string('Stazione_di_partenza', 20);
+            $table->string('Stazione_di_arrivo', 20);
+            $table->float('Orario_di_partenza', 5, 2);
+            $table->float('Orario_di_arrivo', 5, 2);
+            $table->string('Codice_treno', 10);
+            $table->string('Numero_carrozze');
+            $table->boolean('in_orario');
+            $table->boolean('Cancellato');
             $table->timestamps();
+            
         });
     }
 
